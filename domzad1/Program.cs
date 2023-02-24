028e0123804e02965a3e5c816c8d93eb53b26724
Tatevik8 <tvk25@bk.ru>
2023-02-24 22:25:18 +0300

    User enter number and programm write if the number is palindrome.

---
+++ domzad1/Program.cs
@@ -0,0 +1,28 @@
+﻿// **Задача 19**
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.Write("Введите пятизначное число ");
+string? number = Console.ReadLine();
+
+if (number!.Length > 5)
+{
+    System.Console.WriteLine("Вы ввели не пятизначное число");
+}
+else if (number!.Length < 5)
+{
+    System.Console.WriteLine("Вы ввели не пятизначное число");
+}
+else
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Введенное вами число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Введенное вами число НЕ является палиндромом");
+    }
+}
